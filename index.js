04748ac3675ed0e4bcba22c5d25cf9eb29742c39
anamoraisdev <anamorais.dev@gmail.com>
2022-11-10 20:50:53 -0300

    Estrutura e teste

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
 
-//??? button.addEventListener('click', calcular) ????
-
-function possoCalcular(){
-    const pValor = document.getElementById("valor1")
-    const sValor = document.getElementById("valor2")
+//??? button.addEventListener('click', calcular) ???
+function calcular(){
+    const primeiroValor = document.getElementById("valor1")
+    const secundoValor = document.getElementById("valor2")
     const operadores = document.getElementById("operador")
     const button = document.getElementById("calcular")
     const resultado = document.getElementById("resultado")
     const op = operadores.value
+    const v1 = parseFloat(primeiroValor.value)
+    const v2 = parseFloat(secundoValor.value)
 
+    if (op == "+" || op == "-"|| op == "*" || op == "/" ){
+        let resultado;
+        switch (op){
+            case "+":
+                resultado = v1 + v2
+                break;
+            case "-":
+                resultado = v1 - v2
+                break;
+            case "*":
+                resultado = v1 * v2 
+                break;
+            case "/":
+                resultado = v1 / v2 
+                break;
+        }
+        console.log(resultado)
+
+    }else {
+        console.log("Error!")
+    } 
 }
+calcular()
