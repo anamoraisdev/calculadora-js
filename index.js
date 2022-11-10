21c965a738746330b1fd1a13c7a85da606ec2a4f
anamoraisdev <anamorais.dev@gmail.com>
2022-11-10 20:17:14 -0300

    declarando constantes

---
+++ index.js
@@ -0,0 +1,12 @@
+
+//??? button.addEventListener('click', calcular) ????
+
+function possoCalcular(){
+    const pValor = document.getElementById("valor1")
+    const sValor = document.getElementById("valor2")
+    const operadores = document.getElementById("operador")
+    const button = document.getElementById("calcular")
+    const resultado = document.getElementById("resultado")
+    const op = operadores.value
+
+}
